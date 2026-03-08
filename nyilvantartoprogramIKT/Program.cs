a41f5ec8e475cdc8d3d540496620c82b1d5a414c
Tas <tasvss@gmail.com>
2026-03-08 11:36:01 +0100

    végtelen ciklus mig nem helyes szamot adunk meg + menü cicoma

--- nyilvantartoprogramIKT/Program.cs
+++ nyilvantartoprogramIKT/Program.cs
@@ -6,29 +6,39 @@ namespace nyilvantartoprogramIKT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asd");
-            ConsoleKeyInfo kivalasztottBillentyu= Console.ReadKey(true);
-            char lenyomottBillentyu = kivalasztottBillentyu.KeyChar;
-            switch (lenyomottBillentyu)
+            bool menuValaszt = false;
+            while (!menuValaszt)
             {
-                case '1':
-                    Console.Clear();
-                    Console.WriteLine("Menü 1,");
-                    break;
-                case '2':
-                    Console.Clear();
-                    Console.WriteLine("Menü 2,");
-                    break;
-                case '3':
-                    Console.Clear();
-                    Console.WriteLine("Menü 3,");
-                    break;
-
-                default:
-                    Console.WriteLine("Kérjük válasszon a fenti számokból!");
-                    break;
+                Console.WriteLine("----------Üdvözlünk a menüben!----------");
+                Console.WriteLine($"\t1, Menü");
+                Console.WriteLine($"\t2, Menü");
+                Console.WriteLine($"\t3, Menü");
+                Console.WriteLine("----------------------------------------");
+                ConsoleKeyInfo kivalasztottBillentyu = Console.ReadKey(true);
+                char lenyomottBillentyu = kivalasztottBillentyu.KeyChar;
+                switch (lenyomottBillentyu)
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.WriteLine("Menü 1,");
+                        menuValaszt = true;
+                        break;
+                    case '2':
+                        Console.Clear();
+                        Console.WriteLine("Menü 2,");
+                        menuValaszt = true;
+                        break;
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("Menü 3,");
+                        menuValaszt = true;
+                        break;
+                    default:
+                        Console.WriteLine("Kérjük válasszon a fenti számokból!");
+                        Console.Clear();
+                        break;
+                }
             }
-            
         }
     }
 }
