12c46670f94c92bdd937f63dee5c4d1577d0921c
Tas <tasvss@gmail.com>
2026-03-09 18:20:28 +0100

    Menürendszer bővítése

--- nyilvantartoprogramIKT/Program.cs
+++ nyilvantartoprogramIKT/Program.cs
@@ -6,36 +6,62 @@ namespace nyilvantartoprogramIKT
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("Adatlap.txt");
             bool menuValaszt = false;
+            Console.WriteLine("Kérjük válasszon a lenti Menüpontokból!");
+            Console.WriteLine("----------Üdvözlünk a PC szervizünk nyilvántartó programjában!----------");
+            Console.WriteLine($"1, Új munkalap létrehozása");
+            Console.WriteLine($"2, Meglévő munkalap módosítása");
+            Console.WriteLine($"3, Meglévő munkalapok listázása");
+            Console.WriteLine($"4, Meglévő munkalap törlése");
+            Console.WriteLine($"5, Mentés és kilépés");
+            Console.WriteLine("------------------------------------------------------------------------");
             while (!menuValaszt)
             {
-                Console.WriteLine("----------Üdvözlünk a menüben!----------");
-                Console.WriteLine($"\t1, Menü");
-                Console.WriteLine($"\t2, Menü");
-                Console.WriteLine($"\t3, Menü");
-                Console.WriteLine("----------------------------------------");
                 ConsoleKeyInfo kivalasztottBillentyu = Console.ReadKey(true);
                 char lenyomottBillentyu = kivalasztottBillentyu.KeyChar;
                 switch (lenyomottBillentyu)
                 {
                     case '1':
                         Console.Clear();
-                        Console.WriteLine("Menü 1,");
-                        menuValaszt = true;
+                        Console.WriteLine("1, Új munkalap létrehozása");
+
+                        Console.WriteLine("Nyomjon meg bármilyen gombot a kilépéshez!");
                         break;
                     case '2':
                         Console.Clear();
-                        Console.WriteLine("Menü 2,");
-                        menuValaszt = true;
+                        Console.WriteLine("2, Meglévő munkalap módosítása");
+
+                        Console.WriteLine("Nyomjon meg bármilyen gombot a kilépéshez!");
                         break;
                     case '3':
                         Console.Clear();
-                        Console.WriteLine("Menü 3,");
+                        Console.WriteLine("3, Meglévő munkalapok listázása");
+
+                        Console.WriteLine("Nyomjon meg bármilyen gombot a kilépéshez!");
+                        break;
+                    case '4':
+                        Console.Clear();
+                        Console.WriteLine("4, Meglévő munkalap törlése");
+
+                        Console.WriteLine("Nyomjon meg bármilyen gombot a kilépéshez!");
+                        break;
+                    case '5':
+                        Console.Clear();
+                        Console.WriteLine("Mentés és kilépés");
+                        sr.Close();
                         menuValaszt = true;
                         break;
                     default:
-                        Console.WriteLine("Kérjük válasszon a fenti számokból!");
                         Console.Clear();
+                        Console.WriteLine($"\n----------Üdvözlünk a PC szervizünk nyilvántartó programjában!----------");
+                        Console.WriteLine($"1, Új munkalap létrehozása");
+                        Console.WriteLine($"2, Meglévő munkalap módosítása");
+                        Console.WriteLine($"3, Meglévő munkalapok listázása");
+                        Console.WriteLine($"4, Meglévő munkalap törlése");
+                        Console.WriteLine($"5, Mentés és kilépés");
+                        Console.WriteLine("------------------------------------------------------------------------");
+                        Console.WriteLine("Kérjük válasszon a fenti Menüpontokból!");
                         break;
                 }
             }
